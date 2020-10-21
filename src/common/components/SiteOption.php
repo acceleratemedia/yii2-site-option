c25f5ba0224976a74961292f94c51f64cde9dde4
BVBAccelm <brian@accelm.com>
2020-10-20 23:09:20 -0400

    alow overries to be stored in application config

--- src/common/components/SiteOption.php
+++ src/common/components/SiteOption.php
@@ -12,6 +12,14 @@ use yii\base\Component;
 class SiteOption extends Component
 {
 	/**
+	 * SiteOption values can be provided here in the key/value format to override
+	 * database values or to permanently configure a value in application 
+	 * configuration without having to store it in the database
+	 * @var array
+	 */
+	public $overrides = [];
+
+	/**
 	 * Holds values for site options with the key as the key and is used to 
 	 * avoid multiple queries to the database
 	 * @var array
@@ -24,6 +32,9 @@ class SiteOption extends Component
 	 */
 	public function get($key, $refresh = false)
 	{
+		if(isset($this->overrides[$key])){
+			return $this->overrides[$key];
+		}
 		if(!isset($this->_cache[$key]) || $refresh){
 			$this->_cache[$key] = Yii::$app->db->createCommand('SELECT `value` FROM `site_option` WHERE`key`=:key')
 				->bindValue(':key', $key)
